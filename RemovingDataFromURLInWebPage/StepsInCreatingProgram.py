7182453ffda53f8072f8a1a12e3f83abe0fe7753
RKPBE <101239646+RKPBE@users.noreply.github.com>
2022-05-19 10:12:52 +0530

    Add files via upload

---
+++ RemovingDataFromURLInWebPage/StepsInCreatingProgram.py
@@ -0,0 +1,8 @@
+'''
+1.Same code as code for adding two number with get method
+2.Due to get metho it shows the data we have entered in url
+3.To hide it we use post method
+4.Go for home.html file in form add metho="post"
+5.And below it add {% cref_token %}
+6.Change the request in def func in views file from GET to POST
+'''
